dbeff1daeee8e0813e21edaef66cb04d1f771ae1
govindbisai-cmyk <govind.bisai@newtonschool.co>
2026-07-08 01:06:40 +0530

    metabase_to_sheets.py

--- metabase_to_sheets.py
+++ metabase_to_sheets.py
@@ -47,6 +47,7 @@ QUERIES = [
     ("Reapplied",              "https://metabase-lierhfgoeiwhr.newtonschool.co/question/10290-reapplied-rfds-all-time"),
     ("Reactivation",           "https://metabase-lierhfgoeiwhr.newtonschool.co/question/10292-reactivation-rfds-all-time"),
     ("Organic Pure",            "https://metabase-lierhfgoeiwhr.newtonschool.co/question/11238-organic-pure-funnel-rfd-details")
+    ("RFD Mece",            "https://metabase-lierhfgoeiwhr.newtonschool.co/question/11567-rfd-details-flagging-perf-organic-others")
 ]
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
